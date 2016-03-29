0dd8d7a0fc49f48ae74c7e991d583c1fc20f159e
TheGabCode <gabgebgib@gmail.com>
2016-03-29 19:49:05 +0800

    added borrowBook method in User.java

--- User.java
+++ User.java
@@ -8,4 +8,23 @@ public class User{
 		this.name = name;
 		borrowedBooks = new ArrayList<Book>();
 	}
+
+	public void borrowBook(ArrayList<Book> books){
+		Scanner sc  = new Scanner(System.in);
+		String bookToBorrow;
+		int i;
+		bookToBorrow = sc.nextLine();
+
+		for(i = 0; i < books.length; i++){
+			if(bookToBorrow == books[i]){
+				System.out.println("Book found!");
+				this.borrowedBooks.add(Book books[i]);
+				books.remove(i);
+				break;
+			}
+
+		}
+
+	}
+
 }
