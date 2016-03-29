a77aa5ad62f662e0100718f9cc75f2b7b3834f5b
[JoemaNequinto] <janequinto@up.edu.ph>
2016-03-29 19:40:24 +0800

    Edit Library.java

--- Library.java
+++ Library.java
@@ -3,7 +3,7 @@ import java.util.HashMap;
 public class Library{
 	private HashMap<String, ArrayList<Book>> bookMap;
 
-	public library(){
+	public Library(){
 		bookMap = new HashMap<String, ArrayList<Book>>();
 	}		
 	
