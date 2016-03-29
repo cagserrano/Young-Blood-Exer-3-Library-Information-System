c40f38e1c73148e23f5e04e91cd956c49159af7a
cagserrano <cagserrano22@gmail.com>
2016-03-29 19:26:34 +0800

    added library.java

---
+++ Library.java
@@ -0,0 +1,10 @@
+import java.util.HashMap;
+
+public class Library{
+	private HashMap<String, ArrayList<Book>> bookMap;
+
+	public library(){
+		bookMap = new HashMap<String, ArrayList<Book>>();
+	}		
+	
+}
