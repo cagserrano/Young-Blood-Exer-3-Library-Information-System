5d9e2f05dd6a25755c2148d5f0757607b03f99d0
[williamescandor] <[wfescandor@up.edu.ph]>
2016-03-29 20:08:57 +0800

    Edited Book.java

--- Book.java
+++ Book.java
@@ -1,8 +1,36 @@
 public class Book{
-	private final String ID;
-	private String TITLE;
-	private String AUTHOR;
-	private int YEAR;
+	private String ID;
+	private String title;
+	private String author;
+	private String year;
+	private String type;
+	
+	public Book(String ID, String title, String author, String year, String type){
+		this.ID=ID;
+		this.title=title;
+		this.author=author;
+		this.year=year;
+		this.type=type;
+	
+	}
+	
+	
+	public String getId(){
+		return this.ID;
+	}
+	
+	public String getTitle(){
+		return this.title;
+	}
+	public String getAuthor(){
+		return this.author;
+	}
+	public String getYear(){
+		return this.year;
+	}
+	public String type(){
+		return this.type;
+	}
 	
 	
 
